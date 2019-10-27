cfe243a14af2459d9272555a3925ac0a44e9ea7b
Metalglove <Mario.lucassen@hotmail.com>
2019-10-27 20:20:26 +0100

    Added initial console project.

---
+++ ScrobbleBot.CLI/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ScrobbleBot.CLI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
